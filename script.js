d251d7ab2bcfc2d0657aae57a944238376237144
Antonin Gautier <antoningautier@outlook.fr>
2023-01-17 16:30:11 +0100

    1.01.03

---
+++ script.js
@@ -0,0 +1,11 @@
+gsap.to(".bgblue", {
+    scale:1.1,
+    transition:3.5,
+    scrollTrigger: {
+        trigger: ".choisis",
+        markers: true,
+        start: "top 75%",
+        end: "bottom 90%",
+    }
+
+})
