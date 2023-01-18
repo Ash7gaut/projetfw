5f8c7820c643e13e1a4b0f8a369343103a18d1cd
Antonin Gautier <antoningautier@outlook.fr>
2023-01-18 09:34:28 +0100

    1.01.05

--- script.js
+++ script.js
@@ -14,9 +14,157 @@ gsap.from("#star1", {
     x:-50,
     y:-50,
     scrollTrigger: {
-        trigger: ".comm",
-        markers: true,
+        trigger: ".comm1",
         start: "top bottom",
         end: "bottom 80%",
+        markers: true,
+        scrub: 1
     }
 })
+
+// gsap.from("#star2", {
+//     x:-50,
+//     y:-50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star3", {
+//     x:-50,
+//     y:-50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+// gsap.from("#star4", {
+//     x:-50,
+//     y:-50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+// gsap.from("#star5", {
+//     x:-50,
+//     y:-50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star6", {
+//     y:50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star7", {
+//     y:50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star8", {
+//     y:50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star9", {
+//     y:50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star10", {
+//     y:50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star11", {
+//     x:50,
+//     y:-50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star12", {
+//     x:50,
+//     y:-50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star13", {
+//     x:50,
+//     y:-50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star14", {
+//     x:50,
+//     y:-50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
+
+// gsap.from("#star15", {
+//     x:50,
+//     y:-50,
+//     scrollTrigger: {
+//         trigger: ".comm",
+//         markers: true,
+//         start: "top bottom",
+//         end: "bottom 80%",
+//     }
+// })
