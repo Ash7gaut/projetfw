c441574b43c78ab4408ba6915d4424f0a978570b
Antonin Gautier <antoningautier@outlook.fr>
2023-01-18 15:20:04 +0100

    1.01.10

--- script.js
+++ script.js
@@ -1,6 +1,235 @@
+
+// Partie Home
+
+
+gsap.fromTo(".actu", {
+    opacity:0,
+    y: 100,
+}, {
+    y: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".actu",
+        start: "top 80%",
+        end: "bottom 80%",
+
+
+    },
+
+})
+
+
+// Partie Actualité
+
+
+gsap.to(".rose", {
+    autoAlpha:1,
+    transition:1.5,
+    scrollTrigger: {
+        trigger: ".rose",
+        // markers: true,
+        start: "top 75%",
+        end: "bottom 90%",
+    }
+
+})
+
+gsap.to(".godance", {
+    autoAlpha:1,
+    transition:1.5,
+    scrollTrigger: {
+        trigger: ".rose",
+        // markers: true,
+        start: "top 75%",
+        end: "bottom 90%",
+    }
+
+})
+
+
+// Partie Encadré par les meilleurs
+
+
+gsap.fromTo(".encadre", {
+    opacity:0,
+    y:-550,
+}, {
+    y: 0,
+    opacity:1,
+    duration:1.2,
+    scrollTrigger: {
+        trigger: ".photo-1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
+
+
+
+gsap.fromTo(".photo-1", {
+    opacity:0,
+    x: -450,
+}, {
+    x: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".photo-6",
+        start: "top 80%",
+        end: "bottom 80%",
+
+    },
+
+})
+
+gsap.fromTo(".photo-2", {
+    opacity:0,
+    x: -450,
+}, {
+    x: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".photo-6",
+        start: "top 80%",
+        end: "bottom 80%",
+
+    },
+ 
+
+})
+
+gsap.fromTo(".photo-3", {
+    opacity:0,
+    x: -450,
+}, {
+    x: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".photo-6",
+        start: "top 80%",
+        end: "bottom 80%",
+
+    },
+})
+
+gsap.fromTo(".photo-4", {
+    opacity:0,
+    x: 450,
+}, {
+    x: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".photo-9",
+        start: "top bottom",
+        end: "bottom 80%",
+
+    },
+})
+
+gsap.fromTo(".photo-5", {
+    opacity:0,
+    x: 450,
+}, {
+    x: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".photo-9",
+        start: "top bottom",
+        end: "bottom 80%",
+
+    },
+})
+
+gsap.fromTo(".photo-6", {
+    opacity:0,
+    x: 450,
+}, {
+    x: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".photo-9",
+        start: "top bottom",
+        end: "bottom 80%",
+
+    },
+})
+
+gsap.fromTo(".photo-7", {
+    opacity:0,
+    x: -450,
+}, {
+    x: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".photo-9",
+        start: "top bottom",
+        end: "bottom 80%",
+
+    },
+})
+
+gsap.fromTo(".photo-8", {
+    opacity:0,
+    x: -450,
+}, {
+    x: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".photo-9",
+        start: "top bottom",
+        end: "bottom 80%",
+
+    },
+})
+
+gsap.fromTo(".photo-9", {
+    opacity:0,
+    x: -450,
+}, {
+    x: 0,
+    duration:1.5, 
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".photo-9",
+        start: "top bottom",
+        end: "bottom 80%",
+
+    },
+})
+
+
+// Partie Choisis ton plan
+
+
+gsap.fromTo(".choisis", {
+    opacity:0,
+    x:-50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    opacity:1,
+    duration:1.2,
+    scrollTrigger: {
+        trigger: "#mensuel",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
+
 gsap.to(".bgblue", {
-    scale:1.1,
-    transition:3.5,
+    scale:1.14,
+    transition:4.5,
     scrollTrigger: {
         trigger: ".choisis",
         // markers: true,
@@ -10,161 +239,342 @@ gsap.to(".bgblue", {
 
 })
 
-gsap.from("#star1", {
+gsap.to("#mensuel", {
+    backgroundColor: "#E1E1E1",
+    transition:5,
+
+})
+
+
+// Partie Vibz aussi sur mobile
+
+
+gsap.to(".vibzmobile", {
+    autoAlpha: 1,
+    duration:4,
+    scrollTrigger: {
+        trigger: ".vibzmobile"
+    }
+
+})
+
+gsap.fromTo(".apples", {
+    x: -250,
+}, {
+    x: 0,
+    duration:2.5, 
+    scrollTrigger: {
+        trigger: ".apples",
+        start: "top 80%",
+        end: "bottom 80%",
+    },
+
+})
+
+gsap.fromTo(".googlep", {
+    x: 250,
+}, {
+    x: 0,
+    duration:2.5, 
+    scrollTrigger: {
+        trigger: ".apples",
+        start: "top 80%",
+        end: "bottom 80%",
+    },
+})
+
+
+
+gsap.to(".sstr1", {
+    autoAlpha: 1,
+    duration:4,
+    scrollTrigger: {
+        trigger: ".vibzmobile"
+    }
+
+})
+
+
+gsap.fromTo(".sstr", {
+    opacity:0,
+    x:50,
+    y:50,
+}, {
+    x: 0,
+    y: 0,
+    opacity:1,
+    duration:1.2,
+    scrollTrigger: {
+        trigger: "#mensuel",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
+
+
+// Logo entreprise + Avis/Commentaires
+
+
+gsap.to(".logoentr", {
+    autoAlpha: 1,
+    duration:4.5,
+    scrollTrigger: {
+        trigger: ".logoentr"
+    }
+
+})
+
+gsap.fromTo("#star1", {
+    opacity:0,
     x:-50,
     y:-50,
+}, {
+    x: 0,
+    y: 0,
+    opacity:1,
+    duration:1.2,
     scrollTrigger: {
         trigger: ".comm1",
         start: "top bottom",
         end: "bottom 80%",
-        markers: true,
-        scrub: 1
+        // markers: true,
     }
 })
 
-// gsap.from("#star2", {
-//     x:-50,
-//     y:-50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star2", {
+    opacity:0,
+    x:-50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    duration:1.2,
+    delay:0.3,
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star3", {
-//     x:-50,
-//     y:-50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
-// gsap.from("#star4", {
-//     x:-50,
-//     y:-50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
-// gsap.from("#star5", {
-//     x:-50,
-//     y:-50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star3", {
+    opacity:0,
+    x:-50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    duration:1.2,
+    delay:0.6,
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star6", {
-//     y:50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star4", {
+    opacity:0,
+    x:-50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    duration:1.2,
+    delay:0.9,
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star7", {
-//     y:50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star5", {
+    opacity:0,
+    x:-50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    duration:1.2,
+    delay:1.2,
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star8", {
-//     y:50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star6", {
+    opacity:0,
+    x:72,
+}, {
+    x: 0,
+    duration:2,
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star9", {
-//     y:50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star7", {
+    opacity:0,
+    x:36,
+}, {
+    x: 0,
+    duration:2,
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star10", {
-//     y:50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star8", {
+    opacity:0,
+    x:0,
+}, {
+    x: 0,
+    duration:1.5,
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star11", {
-//     x:50,
-//     y:-50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
 
-// gsap.from("#star12", {
-//     x:50,
-//     y:-50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star9", {
+    opacity:0,
+    x:-36,
+}, {
+    x: 0,
+    duration:2,
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star13", {
-//     x:50,
-//     y:-50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star10", {
+    opacity:0,
+    x:-72,
+}, {
+    x: 0,
+    duration:2,
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star14", {
-//     x:50,
-//     y:-50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star11", {
+    opacity:0,
+    x:50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    opacity:1,
+    duration:1.2,
+    delay:1.2,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
 
-// gsap.from("#star15", {
-//     x:50,
-//     y:-50,
-//     scrollTrigger: {
-//         trigger: ".comm",
-//         markers: true,
-//         start: "top bottom",
-//         end: "bottom 80%",
-//     }
-// })
+gsap.fromTo("#star12", {
+    opacity:0,
+    x:50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    opacity:1,
+    duration:1.2,
+    delay:0.9,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
+
+gsap.fromTo("#star13", {
+    opacity:0,
+    x:50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    opacity:1,
+    duration:1.2,
+    delay:0.6,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
+
+gsap.fromTo("#star14", {
+    opacity:0,
+    x:50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    opacity:1,
+    duration:1.2,
+    delay:0.3,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
+
+gsap.fromTo("#star15", {
+    opacity:0,
+    x:50,
+    y:-50,
+}, {
+    x: 0,
+    y: 0,
+    opacity:1,
+    duration:1.2,
+    scrollTrigger: {
+        trigger: ".comm1",
+        start: "top bottom",
+        end: "bottom 80%",
+        // markers: true,
+    }
+})
